c2fc2a6499b0bc2b27a8fcf3d396b46844f7e2e7
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-12-25 17:21:31 -0500

    moved markdown to partials

--- _js/home.js
+++ _js/home.js
@@ -11,8 +11,6 @@
   // ============================================
   $(function() {
 
-
-    console.log(true);
     
 
 
@@ -45,10 +43,10 @@
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
         && location.hostname == this.hostname) {
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        target = target.length ? target : $('id=' + this.hash.slice(1));
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top - $('nav').height()
+            scrollTop: target.offset().top
           }, 500);
           return false;
         }
